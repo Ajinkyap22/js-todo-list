ef39edd14aa48de22109a45b298889b06a2641bc
Ajinkyap22 <palaskarap15@gmail.com>
2021-02-26 20:23:16 +0530

    added local storage support

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 "use script";
 
-const btnEnter = document.getElementById("enter");
+// VARIABLES
+
 const userInput = document.getElementById("user-input");
+const list = document.getElementById("list");
 // Array to store activity object
-const listItems = [];
+let listItems = [];
+getLocalStorage();
+
+// EVENT LISTENERS
+
+// Add activity to list on form submit
+document.querySelector(".add-item").addEventListener("submit", addListElement);
+
+// Handler for delete & complete events
+list.addEventListener("click", updateList);
+
+// FUNCTIONS
 
 // function to create & push activity object in list array
-const addListElement = function (text) {
+function addListElement(e) {
+  e.preventDefault();
+
   const activity = {
     text,
     id: Date.now(),
+    done: false,
   };
 
   listItems.push(activity);
+  setLocalStorage();
   renderListElement(activity);
-};
+  this.reset();
+}
 
 // Function to render the list item
-const renderListElement = function (activity) {
+function renderListElement(activity) {
   // List parent element
   const list = document.getElementById("list");
   // Create a new list node
@@ -27,39 +45,17 @@ const renderListElement = function (activity) {
   node.setAttribute("data-key", activity.id);
 
   node.innerHTML = `
-    <label for="${activity.id}" class="tick"></label>
+    <label for="${activity.id}" class="tick ${
+    activity.done ? "done" : ""
+  }"></label>
     <span>${activity.text}</span>
     <button class="btn-delete">X</button>`;
 
   // Append list node to the list
   list.append(node);
-};
+}
 
-// Function to display the rendered list
-const displayList = function () {
-  // Trim any whitespaces
-  const activityText = userInput.value.trim();
-  // Empty input
-  if (!activityText) return;
-  addListElement(activityText);
-  // Clear input field
-  userInput.value = "";
-  userInput.focus();
-};
-
-// Add activity to list on clicking enter button
-btnEnter.addEventListener("click", displayList);
-
-// Add activity to list on pressing enter
-userInput.addEventListener("keydown", function (e) {
-  // 13 = keycode for enter
-  if (e.keyCode !== 13) return;
-  displayList();
-});
-
-const list = document.getElementById("list");
-// Handler for delete & complete events
-list.addEventListener("click", function (e) {
+function updateList(e) {
   if (e.target.classList.contains("todo-item")) {
     // Mark the activity as done
     e.target.classList.toggle("done");
@@ -74,4 +70,26 @@ list.addEventListener("click", function (e) {
     // Remove acitivity from the array
     listItems.splice(index, 1);
   }
-});
+
+  setLocalStorage();
+}
+
+// To persist data even after reload
+function setLocalStorage() {
+  localStorage.setItem("todolist", JSON.stringify(listItems));
+}
+
+function getLocalStorage() {
+  const data = JSON.parse(localStorage.getItem("todolist"));
+  if (!data) return;
+
+  listItems = data;
+  listItems.forEach((item) => {
+    renderListElement(item);
+  });
+}
+
+// CLear local storage data
+function clearList() {
+  localStorage.clear("todolist");
+}
